5e7d3e7fefa80c5db3e4523be3502926235d0ae0
zhangyusai <739918602@qq.com>
2018-03-21 14:54:44 +0800

    https配置

--- src/main/java/com/zys/elm/ElmApplication.java
+++ src/main/java/com/zys/elm/ElmApplication.java
@@ -1,8 +1,10 @@
 package com.zys.elm;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
@@ -10,5 +12,18 @@ public class ElmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElmApplication.class, args);
+	}
+
+	/**
+	 * it's for set http url auto change to https
+	 */
+	@Bean
+	public Connector httpConnector(){
+		Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(80);
+		connector.setSecure(false);
+		connector.setRedirectPort(443);
+		return connector;
 	}
 }
