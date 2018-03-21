5ee8090ed6cc1ffff3333844c0b947258cdd664e
zhangyusai <739918602@qq.com>
2018-03-21 10:25:41 +0800

    百度统计

---
+++ src/main/resources/static/js/monitor.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?d29fe3035604e869eea52730163ebc58";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
